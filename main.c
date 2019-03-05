4b57944223f1e788fb2d6fa62d86cb49d5a119b7
olesgedz <olesgedz@rambler.ru>
2019-03-06 02:15:08 +0300

    map reader

--- main.c
+++ main.c
@@ -2,15 +2,25 @@
 #include "SDL2/SDL.h"
 #include "libft.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 #define WIN_H 780
 #define WIN_W 780
+
+typedef struct s_player
+{
+	float x;
+	float y;
+	float angle;
+} t_player;
+
 
 typedef struct s_game
 {
 		int				m_bRunning;
 		SDL_Window		*m_pWindow;
 		SDL_Renderer	*m_pRenderer;
+
 }	t_game;
 
 typedef struct s_color
@@ -19,6 +29,24 @@ typedef struct s_color
 	int g;
 	int b;
 } t_color;
+
+typedef struct		s_vector
+{
+	double		x;
+	double		y;
+	double		z;
+	int			color;
+}					t_vector;
+typedef struct		s_map
+{
+	int			width;
+	int			height;
+	int			depth_min;
+	int			depth_max;
+	t_vector	**vectors;
+	double		*colors;
+	int			ncolor;
+}					t_map;
 
 typedef struct			s_line
 {
@@ -34,6 +62,18 @@ typedef struct			s_line
 /********
 
 *******/
+int			ft_error(char *reason)
+{
+	ft_putendl(reason);
+	exit(-1);
+	return (1);
+}
+
+t_vector			ft_vector_at(t_map *map, int x, int y)
+{
+	return (*map->vectors[y * map->width + x]);
+}
+
 
 t_color		*ft_get_rgb(int color)
 {
@@ -52,7 +92,6 @@ void ft_image_set_pixel(t_game *game,  int x, int y, int color)
 
 	rgb = ft_get_rgb(color);
 	SDL_SetRenderDrawColor(game->m_pRenderer, 255, rgb->r, rgb->g, rgb->b);
-	printf("x%d y%d\n", x, y);
 	SDL_RenderDrawPoint(game->m_pRenderer, x, y);
 	SDL_SetRenderDrawColor(game->m_pRenderer, 255, 255, 255, 255);
 }
@@ -208,19 +247,193 @@ void		clean(t_game *game)
 }
 
 
-// int			main()
-// {
-// 	t_game *game;
-// 	game = init(game);
-// 	while(game->m_bRunning)
-// 	{
-// 		handleEvents(game);
-// 		update();
-// 		render(game);
-// 		SDL_Delay(10);
-// 	}
-// 	clean(game);
-// }
+t_vector			*ft_get_vector(int x, int y, int z)
+{
+	t_vector	*v;
+
+	v = ft_memalloc(sizeof(t_vector));
+	if (v == NULL)
+		return (NULL);
+	v->x = (double)x;
+	v->y = (double)y;
+	v->z = (double)z;
+	v->color = 0xFFFFFF;
+	return (v);
+}
+
+static int				ft_cleanup(t_list **lst, t_map **map)
+{
+	t_list	*next;
+
+	while (*lst)
+	{
+		next = (*lst)->next;
+		ft_memdel(&(*lst)->content);
+		ft_memdel((void **)lst);
+		*lst = next;
+	}
+	if (map && *map)
+	{
+		ft_memdel((void **)&(*map)->vectors);
+		ft_memdel((void **)map);
+	}
+	return (0);
+}
+
+
+void					ft_printMap(t_map *map)
+{
+	int j = 0;
+	int k = 0;
+	while (j < map->height)
+	{
+		while (k < map->width)
+		{
+			if (0 <= ABS(ft_vector_at(map, k, j).z)
+ && ABS(ft_vector_at(map, k, j).z) <= 9)
+				printf("%0.f  ", ft_vector_at(map, k, j).z);
+			else
+				printf("%0.f ", ft_vector_at(map, k, j).z);
+			k++;
+		}
+		printf("\n");
+		k = 0;
+		j++;
+	}
+}
+
+
+
+
+static int				ft_populate_map(t_map **m, t_list *list)
+{
+	t_list	*lst;
+	char	**split;
+	int		x;
+	int		y;
+
+	lst = list;
+	y = -1;
+	while (++y < (*m)->height)
+	{
+		x = 0;
+		if ((split = ft_strsplit(lst->content, ' ')) == NULL)
+			return (ft_cleanup(&list, m));
+		while (x < (*m)->width)
+		{
+			(*m)->vectors[y * (*m)->width + x] =
+				ft_get_vector(x, y, ft_atoi(split[x]));
+			x++;
+		}
+		ft_2darrayclean(&split);
+		lst = lst->next;
+	}
+	ft_cleanup(&list, NULL);
+	return (1);
+}
+int			ft_check_line(char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((!ft_isdigit(s[i])) && s[i] != '+' && s[i] != '-'
+		&& !ft_is_space(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+
+
+static int				ft_get_lines(int fd, t_list **lst)
+{
+	t_list	*temp;
+	int		expected;
+	char	*line;
+	int		ret;
+
+	expected = -1;
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		if (expected == -1)
+			expected = (int)ft_countwords(line, ' ');
+		temp = ft_lstnew(line, ft_strlen(line) + 1);
+		if ((temp) == NULL || !ft_check_line(line))
+			return (ft_cleanup(lst, NULL));
+		ft_lstadd(lst, temp);
+		if (expected != (int)ft_countwords(line, ' '))
+			return (ft_cleanup(lst, NULL));
+		ft_strdel(&line);
+	}
+	if (expected == -1 || ret == -1)
+		return (ft_cleanup(lst, NULL));
+	ft_lstrev(lst);
+	return (1);
+}
+
+t_map				*get_map(int width, int height)
+{
+	t_map	*map;
+
+	map = ft_memalloc(sizeof(t_map));
+	if (map == NULL)
+		return (NULL);
+	map->width = width;
+	map->height = height;
+	map->depth_min = 0;
+	map->depth_max = 0;
+	map->vectors = ft_memalloc(sizeof(t_vector *) * width * height);
+	if (map->vectors == NULL)
+	{
+		ft_memdel((void **)&map);
+		return (NULL);
+	}
+	return (map);
+}
+
+
+
+int						ft_read_file(int fd, t_map **m)
+{
+	t_list	*lst;
+
+	lst = NULL;
+	if (!(ft_get_lines(fd, &lst)))
+		return (0);
+	*m = get_map(ft_countwords(lst->content, ' '), ft_lstcount(lst));
+	if (*m == NULL)
+		return (ft_cleanup(&lst, m));
+	return (ft_populate_map(m, lst));
+}
+
+
+
+int			main(int argc, char **argv)
+{
+	t_map	*map;
+	t_game *game;
+	t_player player;
+	int		fd;
+	fd = open(argv[1], O_RDONLY);
+	if (argc != 2)
+		return (ft_error("usage:./wolf3d map"));
+	if (fd < 0 || !ft_read_file(fd, &map))
+		return (ft_error("error: invalid file"));
+	ft_printMap(map);
+	game = init(game);
+
+	while(game->m_bRunning)
+	{
+		handleEvents(game);
+		update();
+		render(game);
+		SDL_Delay(10);
+	}
+	clean(game);
+}
 
 
 
