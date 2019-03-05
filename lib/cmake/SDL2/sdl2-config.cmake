a503cdec693dda90ec53699b01bc622272b123df
olesgedz <olesgedz@rambler.ru>
2019-03-05 18:16:58 +0300

    clean up

--- lib/cmake/SDL2/sdl2-config.cmake
+++ lib/cmake/SDL2/sdl2-config.cmake
@@ -1,10 +1,10 @@
 # sdl2 cmake project-config input for ./configure scripts
 
-set(prefix "/Users/olesgedz/Documents/GitHub/Wolf3D") 
+set(prefix "/Users/jblack-b/Documents/Wolf3D") 
 set(exec_prefix "${prefix}")
 set(libdir "${exec_prefix}/lib")
-set(SDL2_PREFIX "/Users/olesgedz/Documents/GitHub/Wolf3D")
-set(SDL2_EXEC_PREFIX "/Users/olesgedz/Documents/GitHub/Wolf3D")
+set(SDL2_PREFIX "/Users/jblack-b/Documents/Wolf3D")
+set(SDL2_EXEC_PREFIX "/Users/jblack-b/Documents/Wolf3D")
 set(SDL2_LIBDIR "${exec_prefix}/lib")
 set(SDL2_INCLUDE_DIRS "${prefix}/include/SDL2")
 set(SDL2_LIBRARIES "-L${SDL2_LIBDIR}  -lSDL2")
