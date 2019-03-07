0aa63f83096a6568061094c54ba39861a9f4469a
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-03-07 23:43:10 +0300

    1

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -90,7 +90,11 @@ typedef struct s_const
 typedef struct	s_wolf
 {
 	t_map map;
+<<<<<<< HEAD
 	t_sdl *sdl;
+=======
+	t_sdl	game;
+>>>>>>> parent of c864b8c... Revert "1"
 	t_player pl;
 	t_const c;
 	int	fd;
@@ -106,7 +110,7 @@ typedef struct	s_wolf
 
 
 //Utilities.c
-int			ft_error(char *reason);
+int		ft_error(char *reason);
 //first.c
 void    ft_init_wolf(t_wolf *wolf);
 void    ft_ver_line(int x, int start, int end, int color, t_sdl *game);
