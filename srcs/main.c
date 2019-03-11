93a8e531cd1cf66600dd9d0ffea8329b4fed7528
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-03-11 12:47:03 +0300

    IT IS FINALLY WORKING THANK YOU OH LORD

--- srcs/main.c
+++ srcs/main.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lsandor- <lsandor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/08 17:45:04 by lsandor-          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/03/10 22:23:59 by lsandor-         ###   ########.fr       */
-=======
-/*   Updated: 2019/03/10 23:39:11 by jblack-b         ###   ########.fr       */
->>>>>>> master
+/*   Updated: 2019/03/11 12:46:15 by lsandor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,12 +147,8 @@ t_sdl		*init(t_sdl *sdl)
 	sdl->m_bRunning = 1;
 	return (sdl);
 }
-<<<<<<< HEAD
-void		ft_render(t_wolf *wolf)
-=======
 
 int			ft_load_texture(SDL_Renderer *renderer, char *path,SDL_Surface **texture_map,  int id)
->>>>>>> master
 {
 	if (!(texture_map[id] = SDL_LoadBMP(path))) 
 		ft_error("Can't load an image");
